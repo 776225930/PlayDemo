a1483b5a228bfff1e4ecdfcaec9e671f90226a1c
JiangHao <776225930@qq.com>
2020-04-28 09:49:15 +0800

    Hookdemo

--- hookdemo/src/main/java/com/example/hookdemo/HookNotifitionActivity.java
+++ hookdemo/src/main/java/com/example/hookdemo/HookNotifitionActivity.java
@@ -2,13 +2,26 @@ package com.example.hookdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationManager;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
+/**
+ * @author JiangHao
+ * @date 2020/4/2
+ * @describe HooK Notification
+ */
 public class HookNotifitionActivity extends AppCompatActivity {
+    private NotificationManager mNotificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hook_notifition);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        ClipboardManager
+
     }
 }
