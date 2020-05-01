21e8e56501ccfa42cad991bb37ca753e6a6011f4
BugMaker <776225930@qq.com>
2020-05-01 11:21:00 +0800

    类委托和委托属性

--- thefirstlinecode/src/main/java/com/example/thefirstlinecode/primary/HigerOrderFunction.kt
+++ thefirstlinecode/src/main/java/com/example/thefirstlinecode/primary/HigerOrderFunction.kt
@@ -2,6 +2,7 @@ package com.example.thefirstlinecode.primary
 
 import java.lang.StringBuilder
 import kotlin.math.min
+import kotlin.reflect.KProperty
 
 /**
  * @date   2020/4/29
@@ -45,5 +46,77 @@ fun main() {
         append("End")
     }
     println(stringBuilder.toString())
+
+    //不使用泛型,只能作用于固定类型
+    fun StringBuilder.build(block: StringBuilder.() -> Unit): StringBuilder {
+        block()
+        return this;
+    }
+
+    //使用泛型后
+    fun <T> T.build(block: T.() -> Unit): T {
+        block()
+        return this
+    }
+
+    //类委托和委托属性
+    //委托模式的一般写法,需要手动重写所有方法
+    class MySet<T>(val helperSet: HashSet<T>) : Set<T> {
+
+        override val size: Int
+            get() = helperSet.size
+
+        override fun contains(element: T): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun containsAll(elements: Collection<T>): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun isEmpty(): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun iterator(): Iterator<T> {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+    //使用类委托:类委托核心是将一个类的具体实现交给另一个类完成
+    class MySet1<T>(val heplerSet: HashSet<T>) : Set<T> by heplerSet {
+
+        //重写一个方法
+        override fun isEmpty() = false
+
+        //新增一个方法
+        fun helloWorld() = println("hello kotlin")
+    }
+
+    //属性委托
+    var myClass: MyClass = MyClass();
+    myClass.p = "Tom"
+    println(myClass.p)
 }
 
+
+//委托属性
+class MyClass {
+    var p by Delegate()
+}
+
+class Delegate {
+    var propValue: Any? = null
+    //KProperty<*> 属性操作类。用于获取各种属性相关的值
+    operator fun getValue(myClass: MyClass, prop: KProperty<*>): Any? {
+        println("i am getvalue")
+        return propValue
+
+    }
+
+    operator fun setValue(myClass: MyClass, prop: KProperty<*>, value: Any?) {
+        println("i am setValue")
+        propValue = value
+    }
+}
