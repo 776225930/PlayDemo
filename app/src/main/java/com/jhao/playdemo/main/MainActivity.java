d4e9cd6dfa1eb21d1fb618cf6873ebc5554c3978
BugMaker <776225930@qq.com>
2020-11-05 23:40:01 +0800

    开启minifyEnable、shrinkResources精简代码和资源,使用flavorDimensions、splits精简so文件

--- app/src/main/java/com/jhao/playdemo/main/MainActivity.java
+++ app/src/main/java/com/jhao/playdemo/main/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 
